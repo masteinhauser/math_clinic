0853f6893d9acab5007594562d3d8c46facd0c4b
Myles A. Steinhauser <myles.steinhauser@gmail.com>
2012-01-27 02:00:15 -0600

    Nearly complete Equation Engine, faulty logic still.

--- utils/EquationEngine.js
+++ utils/EquationEngine.js
@@ -1,41 +1,31 @@
+var util = require('util');
+
 // This is the engine used to parse through the user defined equation and create all possible problems for it.
 // These are then selected from randomly to the number of questions to be on the test.
 
-// Equation is the code form to split and build from: [0..9]+[1..5] gives 50 combinations
-//                                                  : [0..9]+[0..9]+[0..9] gives 300 combinations
-//                                                  : [0..9]+[1,5] gives 20 combinations
-var parse = exports.parse = function(equation, count, callback){
+// Equation is the code form to split and build from: [0..9][+][1..5] gives 50 combinations
+//                                                  : [0..9][+-][0..9][+][0..9] gives 2000 combinations
+//                                                  : [0..9][+-][1,5] gives 40 combinations
+var parse = exports.parse = function(equation, callback){
    var eq = [];
-   var numbers = [], operators = [];
    var i = 0;
 
 // Start to parser
-   util.log("Equation: "+eq+'<br/>');
-
-   // Remove operators
-   var s = eq.replace(/[\+\-\*\/]/g, '');
+   util.log("Equation: "+ equation);
 
    // Match number: ranges, etc.
-   var m = s.match(/((?:\[\d[^\]]+\])+)/);
+   var m = equation.match(/((?:\[[^\]]+\])+)/);
 
    // Remove first and last bracket
    var substr = m[1].substring(1, m[1].length - 1);
 
    // Split equation into parts
-   numbers = substr.split("][");
-
-   // TODO: Split correct into array based on ][ brackets and not just operator
-   // TODO: i.e. Add an empty entry for ][ but add proper operator for ]+[ or ]*[
-   // Strip out non-operators
-   var o = eq.replace(/((?:\[\d[^\]]+\])+)/g, '');
-
-   // Split operators into array
-   operators = s.split("");
+   eq = substr.split("][");
 
    if(callback){
-      callback(numbers, operators);
+      callback(eq);
    } else {
-      return [numbers, operators];
+      return eq;
    }
 };
 
@@ -43,9 +33,9 @@ var parse = exports.parse = function(equation, count, callback){
 var range = function(number, callback){
    var range = [];
    var regLower = /\d+/;
-   var regUpper = /\.+\d+/;
+   var regUpper = /\.{2,}\d+/;
    var lower = number.match(regLower)[0]; // Grab first match
-   var upper = number.match(regUpper)[0].replace('.','');
+   var upper = number.match(regUpper)[0].replace('..','');
    var i = 0;
 
    for(i=lower; i<=upper; i++){
@@ -75,58 +65,100 @@ var specific = function(number, callback){
    }
 };
 
-// Generate list of equations from numbers and operators then randomly choose count from total
-var generate = exports.generate = function(numbers, operators, count, callback){
-   var i = 0, j = 0, k = 0;
-   var eq = [];// Array of Arrays
-   var questions = [];
+// Add equation part to all previously built equations
+var build = exports.build = function(eqParts, idx, questions){
+   var i = 0, j = 0;
+   idx = idx || 0;
+   questions = questions || [];
+   util.log("Questions: "+ questions.length);
 
-   var regNumber = /^[\-]?[0-9]+[\.]?[0-9]+$/;
-   var regRange = /\d+.*\d+/;
+   // j: Insert or Append each question part
+   for(i=0; i< eqParts[idx].length; i++){
+      util.log("eqParts["+ idx +"]: "+ eqParts[idx].length);
+
+      var larger = (questions.length > eqParts[idx].length ? questions.length : eqParts[idx].length);
+      for(j=0; j<larger; j++){
+         // Append operator to question
+         questions[j] = (questions[j] || questions[j-1] || "") + eqParts[idx][i];
+      }
+
+      // Recursively call for next item
+      if(idx+1 === eqParts.length){
+         return questions;
+      } else {
+         return build(eqParts, ++idx, questions);
+      }
+   }
+};
+
+var trim = exports.trim = function(questions, count){
+   // TODO: Cut down questions to number specified in count.
+   // Math.floor(rand);
+   //
+   // if(questions.length < count){
+   //    util.log("WARNING: Number of requested questions more than possible from equation.");
+   //    util.log("Possible Combinations: "+ questions.length);
+   //    util.log("Requested Questions: "+ count);
+   // }
+   //
+   //return questions;
+};
+
+// Generate array of parts from numbers and operators
+var generate = exports.generate = function(eq, callback){
+   var i = 0;
+   var parts = [];
+
+   //var regNumber = /^[\-]?[0-9]+[\.]?[0-9]+$/;
+   var regNumber = /^([0-9]*|\d*\.\d{1}?\d*)$/;
+   var regRange = /\d+\.{2,}\d+/;
    var regSpecific = /\d+,*\d+/;
+   var regOperator = /[\+\-\*\/]/;
 
-   // i: For each number part, generate equation and store possibilities
-   for(i=0; i<numbers.length; i++){
+   // i: For each equation part, generate and store possibilities
+   for(i=0; i<eq.length; i++){
       // Always enter switch
-      switch(true){
+      switch(eq.length > 0){
          //Single
-         case regNumber.test(numbers[i]):
-            eq[i] = numbers[i];
+         case regNumber.test(eq[i]):
+            parts.push(eq[i]);
             break;
          // Range
-         case regRange.test(numbers[i]):
-            eq[i] = range(numbers[i]);
+         case regRange.test(eq[i]):
+            parts.push(range(eq[i]));
             break;
          // Specific
-         case regSpecific.test(numbers[i]):
-            eq[i] = specific(numbers[i]);
+         case regSpecific.test(eq[i]):
+            parts.push(specific(eq[i]));
+            break;
+         // Operator
+         case regOperator.test(eq[i]):
+            parts.push(eq[i].split(''));
             break;
          // Error or Unknown
          default:
-            util.log("ERROR: Unknown character used in "+numbers[i]);
+            util.log("ERROR: Unknown character used in "+eq[i]);
             break;
       } // End switch
    } // End number parts
 
-   // TODO: Fix to allow varying length number parts
-   // j: Insert or Append each question part
-   for(j=0; j<eq.length; j++, k<eq.length ? k++ : k = 0){
-      for(k=0; ; k<eq[j].length ? k++ : k = 0){
-
-      }
-      // Append operator to question
-      questions[j] += operators[i];
-
-      // k: If we run out of pieces to append, loop over and continue
-      questions[j] += eq[k];
+   if(callback){
+      callback(parts);
+   } else {
+      return parts;
    }
-
-   return questions;
 };
 
-var equation = '[09]+[1,5]-[0..99]';
+var equation = '[1][0][+][1,5][+][0..99]';
 var count = 10;
 
-parse(equation, count, generate);
+var parsedEquation = parse(equation);
+var parts = generate(parsedEquation);
+var builtQuestions = build(parts);
+var subsetQuestions = trim(builtQuestions, count);
 
+var z = 0;
+for(z = 0; z < builtQuestions.length; z++){
+   util.log(builtQuestions[z]);
+}
 
