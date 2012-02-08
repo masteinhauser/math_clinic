29fa70343a2e3a4995b075edce815b350699453b
Myles A. Steinhauser <myles.steinhauser@gmail.com>
2012-02-08 03:32:30 -0600

    Fix setup

--- setup.js
+++ setup.js
@@ -16,15 +16,7 @@ db.setup(function(err){
       throw error;
    }
 
-   var user = new User();
-
-   user.username = "myles";
-   user.password = "pass";
-   user.role = "admin";
-   user.fname = "Myles";
-   user.lname = "Steinhauser";
-   user.birth = "07/01/1990";
-   user.save(function(err){
+   User.add("myles", "pass", "admin", "Myles", "Steinhauser", "07/01/1990", function(err){
       if(err){
          util.log("ERROR: "+err);
          return err;
