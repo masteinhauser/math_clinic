ccf7c1983aa412e1dbbe6ad5a87f3819f70a676a
Myles A. Steinhauser <myles.steinhauser@gmail.com>
2012-02-12 20:06:49 -0600

    Split into Admin page list

--- routes/engine.js
+++ routes/engine.js
@@ -12,7 +12,8 @@ module.exports = function(app){
 
    app.post('/engine', function(req, res){
       var eq = require('../utils/EquationEngine');
-      var result = eq.run(req.body.eq, req.body.count, req.body.randomize);
+      var randomize = req.body.randomize === "on"? true : false;
+      var result = eq.run(req.body.eq, req.body.count, randomize);
 
       res.render('engine.jade', {
          layout: false,
